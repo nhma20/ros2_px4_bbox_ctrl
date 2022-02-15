1e10f6ed8d5a36e8281a94a649cba71be50513b2
nhma <nhma@mmmi.sdu.dk>
2022-02-15 12:36:01 +0100

    added files, offboard hover+land

---
+++ launch/hover_bbox_ctrl.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='px4_ros_com',
+            #namespace='turtlesim1',
+            executable='vel_ctrl_vec_pub',
+            name='vel_ctrl_vec_pub'
+        ),
+        Node(
+            package='px4_ros_com',
+            #namespace='turtlesim2',
+            executable='lidar_to_mmwave',
+            name='lidar_to_mmwave'
+        )
+    ])
